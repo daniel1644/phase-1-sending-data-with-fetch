bec734370df21c8701d6d3d77a5f857705eba255
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-04-23 15:33:11 +0300

    Sending Data with Fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+const submitData = (name, email) => {
+    const url = 'http://localhost:3000/users';
+    const data = { name, email };
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        const newId = data.id;
+        const newUser = data;
+        document.body.innerHTML += `<p>New user added with id: ${newId}</p>`;
+        document.body.innerHTML += `<p>Name: ${newUser.name}</p>`;
+        document.body.innerHTML += `<p>Email: ${newUser.email}</p>`;
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        document.body.innerHTML += `<p>Error: ${errorMessage}</p>`;
+      });
+  }
